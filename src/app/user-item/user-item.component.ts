e25b3034cd18b235d0fa0332a36b1545313859ea
mohamedanies <modysalem22@yahoo.com>
2020-05-25 04:27:22 +0200

    ok we got overview about angular' '

--- src/app/user-item/user-item.component.ts
+++ src/app/user-item/user-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserItemComponent implements OnInit {
 
-  constructor() { }
+  @Input() name : string;
+
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
   }
